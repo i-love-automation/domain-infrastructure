7e7f0b585095ae8ac1ac1bdec4ba9955770aef99
Terraform <romain.cambonie@gmail.com>
2023-04-14 23:34:08 +0200

    feat: adding terraform aws service file

---
+++ acm.tf
@@ -0,0 +1,22 @@
+resource "aws_acm_certificate" "acm_certificate" {
+  provider          = aws.us-east-1
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = local.tags
+}
+
+resource "aws_acm_certificate_validation" "certification_main" {
+  provider        = aws.us-east-1
+  certificate_arn = aws_acm_certificate.acm_certificate.arn
+  validation_record_fqdns = [
+    aws_route53_record.certificate_validation_main.fqdn,
+  ]
+  timeouts {
+    create = "48h"
+  }
+}
