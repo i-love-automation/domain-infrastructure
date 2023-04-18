ab503473b8fff12a602eb2e33f099c64915bee1d
Terraform <romain.cambonie@gmail.com>
2023-04-18 09:12:50 +0200

    feat: adding terraform aws variables file

--- variables.tf
+++ variables.tf
@@ -2,14 +2,7 @@ variable "project" {
   type        = string
   nullable    = false
   description = "The name of the project that hosts the environment"
-  default     = "PROJECT"
 }
-#variable "service" {
-#  type        = string
-#  nullable    = false
-#  description = "The name of the service that will be run on the environment"
-#  default     = "SERVICE"
-#}
 
 variable "domain_name" {
   type        = string
