42ccddbe19dda7bdb773c025ff70b0f4f8d8b69a
Terraform <noreply@codingones.com>
2023-05-02 16:10:44 +0200

    feat: adding terraform aws service file

--- route53.tf
+++ route53.tf
@@ -38,7 +38,7 @@ resource "aws_route53_record" "main_name_servers_record" {
   depends_on = [aws_route53_zone.hosting_zone]
 }
 
-resource "aws_route53_record" "validation_record" {
+resource "aws_route53_record" "validation_records" {
   for_each = {
     for dvo in aws_acm_certificate.acm_certificate.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
@@ -53,9 +53,4 @@ resource "aws_route53_record" "validation_record" {
   ttl             = 60
   type            = each.value.type
   zone_id         = aws_route53_zone.hosting_zone.zone_id
-}
-
-resource "aws_acm_certificate_validation" "validation" {
-  certificate_arn         = aws_acm_certificate.acm_certificate.arn
-  validation_record_fqdns = [for record in aws_route53_record.validation_record : record.fqdn]
 }
