d37984ebf75a64544d23b7cf63514e639cd7d0f2
Terraform <noreply@codingones.com>
2023-05-02 16:03:46 +0200

    feat: adding terraform aws service file

--- acm.tf
+++ acm.tf
@@ -12,7 +12,7 @@ resource "aws_acm_certificate" "acm_certificate" {
 resource "aws_acm_certificate_validation" "certification_main" {
   certificate_arn = aws_acm_certificate.acm_certificate.arn
   validation_record_fqdns = [
-    aws_route53_record.certificate_validation_main.fqdn,
+    aws_route53_record.validation_record.fqdn,
   ]
   timeouts {
     create = "48h"
